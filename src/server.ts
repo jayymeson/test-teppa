b2481e92d568b47898c5726aeb9ded466bdc2e14
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-08-25 10:06:58 -0300

    feature: intial commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world with Typescript' })
+})
+
+app.use(route)
+
+const port = 3001
+
+app.listen(port, () => {
+  if (!process.env.PORT) {
+    console.log(`API is running 🚀 on port http://localhost:${port}`);
+  }
+});
+
